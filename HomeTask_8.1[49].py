c7447d54f29e877fdc1680bd068f04481520de7c
Ilya Besedin <besedka07@icloud.com>
2023-03-13 19:57:35 +0300

    Urok_8_corrected

--- HomeTask_8.1[49].py
+++ HomeTask_8.1[49].py
@@ -144,15 +144,14 @@ def change_contact(file_name):
     contact_list.remove(number_to_change)
     print('Какое поле вы хотите изменить?')
     field = input('1 - Фамилия\n2 - Имя\n3 - Номер телефона\n')
-
-if field == '1':
+    if field == '1':
         number_to_change[0] = input('Введите фамилию: ')
     elif field == '2':
         number_to_change[1] = input('Введите имя: ')
     elif field == '3':
         number_to_change[2] = input('Введите номер телефона: ')
     contact_list.append(number_to_change)
-      with open(file_name, 'w', encoding='utf-8') as file:
+    with open(file_name, 'w', encoding='utf-8') as file:
         for contact in contact_list:
             line = ' '.join(contact) + '\n'
             file.write(line)
